a1e6af3428c18313070afe867473a40c471e9543
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-17 23:22:22 +0300

    Create transition_block.py

---
+++ src/blocks/transition_block.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class TransitionBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(TransitionBlock, self).__init__()
+        self.transition = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.transition(x)
